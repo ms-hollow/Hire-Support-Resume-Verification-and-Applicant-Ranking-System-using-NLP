b2ec352ac435e3ad8ef6260ba4dc526a3bfb55cc
larajunglau <93086455+larajunglau@users.noreply.github.com>
2024-09-28 19:54:34 +0800

    Added login and register

--- frontend/app/components/GeneralFooter.js
+++ frontend/app/components/GeneralFooter.js
@@ -1,13 +1,41 @@
+"use client";
+import { useEffect, useState } from "react";
+
 const GeneralFooter = () => {
-    return ( 
-        <footer className="bg-fontcolor text-background place-items-end">
-                <p>© 2024 Hire Support. All rights reserved. </p>
-                <ul className="list-disc flex-center">
-                    <li className=" mx-2 ">Privacy Policy</li>
-                    <li className=" mx-4 ">Terms & Condition</li>
-                </ul>       
+    const [isVisible, setIsVisible] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            const scrollY = window.scrollY;
+            const windowHeight = window.innerHeight;
+            const documentHeight = document.documentElement.scrollHeight;
+
+            // Check if scrolled to bottom
+            if (scrollY + windowHeight >= documentHeight - 100) {
+                setIsVisible(true);
+            } else {
+                setIsVisible(false);
+            }
+        };
+
+        window.addEventListener("scroll", handleScroll);
+
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, []);
+
+    return (
+        <div className="pt-36">
+        <footer className={`bg-fontcolor text-background w-full fixed bottom-0 left-0 p-4 items-center ${isVisible ? 'block' : 'hidden'}`}>
+            <p className="lg:text-medium mb:text-small sm:text-small xsm:text-small">© 2024 Hire Support. All rights reserved.</p>
+            <ul className="list-disc space-x-4">
+                <li className="text-background lg:text-medium mb:text-small sm:text-small xsm:text-small">Privacy Policy</li>
+                <li className="text-background lg:text-medium mb:text-small sm:text-small xsm:text-small">Terms & Condition</li>
+            </ul>
         </footer>
-     );
-}
- 
+        </div>
+    );
+};
+
 export default GeneralFooter;
