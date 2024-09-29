f9f5e2cae468c24e7a8f202a1d5edcf7ae1c01c1
larajunglau <93086455+larajunglau@users.noreply.github.com>
2024-09-29 13:34:12 +0800

    added Landing Page

--- frontend/app/(pages)/(GENERAL)/LandingPage/page.js
+++ frontend/app/(pages)/(GENERAL)/LandingPage/page.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <div> 
-      <p>Laica Ygot</p>   
+    <div  className="flex items-center justify-center lg:pt-36 mb:pt-24 xsm:pt-24 sm:pt-24 mb:p-5 sm:p-12 xsm:p-14 py-20 mx-auto"> 
+      <p>This is Landing Page</p>   
     </div>
   );
 }
