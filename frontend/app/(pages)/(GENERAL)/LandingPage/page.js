f6e8772b425c2b94ad91615e9a1e4528f767cb26
larajunglau <93086455+larajunglau@users.noreply.github.com>
2024-11-24 18:37:13 +0800

    Delete frontend directory

--- frontend/app/(pages)/(GENERAL)/LandingPage/page.js
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div  className="flex items-center justify-center lg:pt-36 mb:pt-24 xsm:pt-24 sm:pt-24 mb:p-5 sm:p-12 xsm:p-14 py-20 mx-auto"> 
-      <p>This is Landing Page</p>   
-    </div>
-  );
-}
-
-
