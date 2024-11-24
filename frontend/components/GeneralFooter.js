242d78b6c39494be24586c469d100b88f5844ea4
larajunglau <ygotlaica@gmail.com>
2024-11-24 18:46:00 +0800

    Added Login and Register

---
+++ frontend/components/GeneralFooter.js
@@ -0,0 +1,44 @@
+import { useEffect, useState } from "react";
+
+const GeneralFooter = () => {
+    const [isVisible, setIsVisible] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            const scrollY = window.scrollY;
+            const windowHeight = window.innerHeight;
+            const documentHeight = document.documentElement.scrollHeight;
+
+            // Ensure visibility for short pages
+            if (documentHeight <= windowHeight || scrollY + windowHeight >= documentHeight - 100) {
+                setIsVisible(true);
+            } else {
+                setIsVisible(false);
+            }
+        };
+
+        window.addEventListener("scroll", handleScroll);
+
+        // Initial check in case the page starts at the bottom or is too short
+        handleScroll();
+
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, []);
+
+    return (
+        <div className="pt-10">
+            <footer className={`bg-fontcolor text-background w-full fixed bottom-0 left-0 p-2 flex flex-col items-center ${isVisible ? 'block' : 'hidden'}`}>
+                <p className="lg:text-xsmall mb:text-xsmall sm:text-xsmall xsm:text-xsmall">© 2024 Hire Support. All rights reserved.</p>
+                <ul className="list-disc list-inside flex space-x-4">
+                    <li className="text-background lg:text-xsmall mb:text-xsmall sm:text-xsmall xsm:text-xsmall">Privacy Policy</li>
+                    <li className="text-background lg:text-xsmall mb:text-xsmall sm:text-xsmall xsm:text-xsmall">Terms & Condition</li>
+                </ul>
+         </footer> 
+        </div>
+       
+    );
+};
+
+export default GeneralFooter;
---
+++ frontend/pages/APPLICANT/ApplicationSubmit.js
@@ -0,0 +1,12 @@
+import ApplicantHeader from "@/components/ApplicantHeader";
+
+export default function  ApplicationSubmit () {
+    return ( 
+        <div>
+            <ApplicantHeader/>
+                <div className=" lg:pt-28 mb:pt-24 xsm:pt-24 sm:pt-24 mb:px-20 sm:px-20 xsm:px-20 lg:px-20 mx-auto">
+                    <p>Application Submitted </p>
+                </div>
+        </div>
+     );
+}
