88edbf5cc34e3f3c116e8613223dbe83de00132d
unknown <alexisglenn.aspiras@tup.edu.ph>
2024-12-01 23:42:47 +0800

    Added frontend on page My Jobs

---
+++ frontend/components/SavedJobs.js
@@ -0,0 +1,298 @@
+import { useState } from "react";
+import Image from 'next/image';
+import GeneralHeader from '@/components/GeneralHeader';
+import GeneralFooter from '@/components/GeneralFooter';
+
+const SavedJobs = () => {
+    const [isSaved, setIsSaved] = useState(false);
+    const toggleSave = () => {
+        setIsSaved(!isSaved);
+    };
+
+    return (
+        <div className="flex flex-col">
+            <><GeneralHeader />
+                {/* Saved Job #1 */}
+                <div className="flex flex-col items-center justify-center w-screen pt-6">
+                    <div className="box-container px-8 py-5 ">
+                        <div className="flex flex-row items-center -mt-8">
+                            <Image src="/Logo.png" width={30} height={30} alt="Company Logo" />
+                            <div className="ml-4 mt-8">
+                                <b className="font-bold text-large text-black">Job Title</b>
+                                    <div className="text-xsmall font-thin text-fontcolor mt-1">
+                                        <p>Company</p>
+                                        <p>Job Industry</p>
+                                    </div>
+                            </div>
+                            <div className="text-center ml-40">
+                                <a href="/APPLICANT/JobApplication" className="apply-now-container">Apply Now</a>
+                            </div>
+                            <button onClick={toggleSave} className="ml-auto">
+                                <Image src={isSaved ? "/Save Icon.svg" : "/Unsave Icon.svg"} width={13} height={11} alt={isSaved ? "Save Icon" : "Unsave Icon"} />
+                            </button>
+                        </div>  
+                        <div className="flex flex-row justify-between mt-2 px-11">
+                            <div className="flex flex-row">
+                                <Image src="/Location Icon.svg" width={23} height={20} alt="Location Icon" />
+                                <p id='work_location' className="ml-1.5 font-thin text-xsmall text-fontcolor">Valenzuela</p>
+                            </div>
+                            <div className="flex flex-row mx-4">
+                                <Image src="/Work Setup Icon.svg" width={23} height={20} alt="Work Setup Icon" />
+                                <p id='work_setup' className="ml-2 font-thin text-xsmall text-fontcolor">Remote</p>
+                            </div>
+                            <div className="flex flex-row">
+                                <Image src="/Schedule Icon.svg" width={18} height={20} alt="Schedule Icon" />
+                                <p id='schedule' className="ml-2 font-thin text-xsmall text-fontcolor">8 hrs shift</p>
+                            </div>
+                        </div>
+
+                        <div className="flex flex-row mt-2 px-11">
+                            <Image 
+                                src="/Salary Icon.svg" 
+                                width={18} 
+                                height={20}
+                                alt="Salary Icon"
+                            />
+                            <p id='salary' className="ml-2 font-thin text-xsmall pl-px text-fontcolor">Php 17,000 - 21,000 Monthly</p>
+                        </div>
+                    </div>
+                </div>
+                {/* Saved Job #2 */}
+                <div className="flex flex-col items-center justify-center w-screen pt-4">
+                    <div className="box-container px-8 py-5 ">
+                        <div className="flex flex-row items-center -mt-8">
+                            <Image src="/Logo.png" width={30} height={30} alt="Company Logo" />
+                            <div className="ml-4 mt-8">
+                                <b className="font-bold text-large text-black">Job Title</b>
+                                    <div className="text-xsmall font-thin text-fontcolor mt-1">
+                                        <p>Company</p>
+                                        <p>Job Industry</p>
+                                    </div>
+                            </div>
+                            <div className="text-center ml-40">
+                                <a href="/APPLICANT/JobApplication" className="apply-now-container">Apply Now</a>
+                            </div>
+                            <button onClick={toggleSave} className="ml-auto">
+                                <Image src={isSaved ? "/Save Icon.svg" : "/Unsave Icon.svg"} width={13} height={11} alt={isSaved ? "Save Icon" : "Unsave Icon"} />
+                            </button>
+                        </div>  
+                        <div className="flex flex-row justify-between mt-2 px-11">
+                            <div className="flex flex-row">
+                                <Image src="/Location Icon.svg" width={23} height={20} alt="Location Icon" />
+                                <p id='work_location' className="ml-1.5 font-thin text-xsmall text-fontcolor">Valenzuela</p>
+                            </div>
+                            <div className="flex flex-row mx-4">
+                                <Image src="/Work Setup Icon.svg" width={23} height={20} alt="Work Setup Icon" />
+                                <p id='work_setup' className="ml-2 font-thin text-xsmall text-fontcolor">Remote</p>
+                            </div>
+                            <div className="flex flex-row">
+                                <Image src="/Schedule Icon.svg" width={18} height={20} alt="Schedule Icon" />
+                                <p id='schedule' className="ml-2 font-thin text-xsmall text-fontcolor">8 hrs shift</p>
+                            </div>
+                        </div>
+
+                        <div className="flex flex-row mt-2 px-11">
+                            <Image 
+                                src="/Salary Icon.svg" 
+                                width={18} 
+                                height={20}
+                                alt="Salary Icon"
+                            />
+                            <p id='salary' className="ml-2 font-thin text-xsmall pl-px text-fontcolor">Php 17,000 - 21,000 Monthly</p>
+                        </div>
+                    </div>
+                </div>
+                {/* Saved Job #3 */}
+                <div className="flex flex-col items-center justify-center w-screen pt-4">
+                    <div className="box-container px-8 py-5 ">
+                        <div className="flex flex-row items-center -mt-8">
+                            <Image src="/Logo.png" width={30} height={30} alt="Company Logo" />
+                            <div className="ml-4 mt-8">
+                                <b className="font-bold text-large text-black">Job Title</b>
+                                    <div className="text-xsmall font-thin text-fontcolor mt-1">
+                                        <p>Company</p>
+                                        <p>Job Industry</p>
+                                    </div>
+                            </div>
+                            <div className="text-center ml-40">
+                                <a href="/APPLICANT/JobApplication" className="apply-now-container">Apply Now</a>
+                            </div>
+                            <button onClick={toggleSave} className="ml-auto">
+                                <Image src={isSaved ? "/Save Icon.svg" : "/Unsave Icon.svg"} width={13} height={11} alt={isSaved ? "Save Icon" : "Unsave Icon"} />
+                            </button>
+                        </div>  
+                        <div className="flex flex-row justify-between mt-2 px-11">
+                            <div className="flex flex-row">
+                                <Image src="/Location Icon.svg" width={23} height={20} alt="Location Icon" />
+                                <p id='work_location' className="ml-1.5 font-thin text-xsmall text-fontcolor">Valenzuela</p>
+                            </div>
+                            <div className="flex flex-row mx-4">
+                                <Image src="/Work Setup Icon.svg" width={23} height={20} alt="Work Setup Icon" />
+                                <p id='work_setup' className="ml-2 font-thin text-xsmall text-fontcolor">Remote</p>
+                            </div>
+                            <div className="flex flex-row">
+                                <Image src="/Schedule Icon.svg" width={18} height={20} alt="Schedule Icon" />
+                                <p id='schedule' className="ml-2 font-thin text-xsmall text-fontcolor">8 hrs shift</p>
+                            </div>
+                        </div>
+
+                        <div className="flex flex-row mt-2 px-11">
+                            <Image 
+                                src="/Salary Icon.svg" 
+                                width={18} 
+                                height={20}
+                                alt="Salary Icon"
+                            />
+                            <p id='salary' className="ml-2 font-thin text-xsmall pl-px text-fontcolor">Php 17,000 - 21,000 Monthly</p>
+                        </div>
+                    </div>
+                </div>
+                {/* Saved Job #4 */}
+                <div className="flex flex-col items-center justify-center w-screen pt-4">
+                    <div className="box-container px-8 py-5 ">
+                        <div className="flex flex-row items-center -mt-8">
+                            <Image src="/Logo.png" width={30} height={30} alt="Company Logo" />
+                            <div className="ml-4 mt-8">
+                                <b className="font-bold text-large text-black">Job Title</b>
+                                    <div className="text-xsmall font-thin text-fontcolor mt-1">
+                                        <p>Company</p>
+                                        <p>Job Industry</p>
+                                    </div>
+                            </div>
+                            <div className="text-center ml-40">
+                                <a href="/APPLICANT/JobApplication" className="apply-now-container">Apply Now</a>
+                            </div>
+                            <button onClick={toggleSave} className="ml-auto">
+                                <Image src={isSaved ? "/Save Icon.svg" : "/Unsave Icon.svg"} width={13} height={11} alt={isSaved ? "Save Icon" : "Unsave Icon"} />
+                            </button>
+                        </div>  
+                        <div className="flex flex-row justify-between mt-2 px-11">
+                            <div className="flex flex-row">
+                                <Image src="/Location Icon.svg" width={23} height={20} alt="Location Icon" />
+                                <p id='work_location' className="ml-1.5 font-thin text-xsmall text-fontcolor">Valenzuela</p>
+                            </div>
+                            <div className="flex flex-row mx-4">
+                                <Image src="/Work Setup Icon.svg" width={23} height={20} alt="Work Setup Icon" />
+                                <p id='work_setup' className="ml-2 font-thin text-xsmall text-fontcolor">Remote</p>
+                            </div>
+                            <div className="flex flex-row">
+                                <Image src="/Schedule Icon.svg" width={18} height={20} alt="Schedule Icon" />
+                                <p id='schedule' className="ml-2 font-thin text-xsmall text-fontcolor">8 hrs shift</p>
+                            </div>
+                        </div>
+
+                        <div className="flex flex-row mt-2 px-11">
+                            <Image 
+                                src="/Salary Icon.svg" 
+                                width={18} 
+                                height={20}
+                                alt="Salary Icon"
+                            />
+                            <p id='salary' className="ml-2 font-thin text-xsmall pl-px text-fontcolor">Php 17,000 - 21,000 Monthly</p>
+                        </div>
+                    </div>
+                </div>
+                {/* Saved Job #5 */}
+                <div className="flex flex-col items-center justify-center w-screen pt-4">
+                    <div className="box-container px-8 py-5 ">
+                        <div className="flex flex-row items-center -mt-8">
+                            <Image src="/Logo.png" width={30} height={30} alt="Company Logo" />
+                            <div className="ml-4 mt-8">
+                                <b className="font-bold text-large text-black">Job Title</b>
+                                    <div className="text-xsmall font-thin text-fontcolor mt-1">
+                                        <p>Company</p>
+                                        <p>Job Industry</p>
+                                    </div>
+                            </div>
+                            <div className="text-center ml-40">
+                                <a href="/APPLICANT/JobApplication" className="apply-now-container">Apply Now</a>
+                            </div>
+                            <button onClick={toggleSave} className="ml-auto">
+                                <Image src={isSaved ? "/Save Icon.svg" : "/Unsave Icon.svg"} width={13} height={11} alt={isSaved ? "Save Icon" : "Unsave Icon"} />
+                            </button>
+                        </div>  
+                        <div className="flex flex-row justify-between mt-2 px-11">
+                            <div className="flex flex-row">
+                                <Image src="/Location Icon.svg" width={23} height={20} alt="Location Icon" />
+                                <p id='work_location' className="ml-1.5 font-thin text-xsmall text-fontcolor">Valenzuela</p>
+                            </div>
+                            <div className="flex flex-row mx-4">
+                                <Image src="/Work Setup Icon.svg" width={23} height={20} alt="Work Setup Icon" />
+                                <p id='work_setup' className="ml-2 font-thin text-xsmall text-fontcolor">Remote</p>
+                            </div>
+                            <div className="flex flex-row">
+                                <Image src="/Schedule Icon.svg" width={18} height={20} alt="Schedule Icon" />
+                                <p id='schedule' className="ml-2 font-thin text-xsmall text-fontcolor">8 hrs shift</p>
+                            </div>
+                        </div>
+
+                        <div className="flex flex-row mt-2 px-11">
+                            <Image 
+                                src="/Salary Icon.svg" 
+                                width={18} 
+                                height={20}
+                                alt="Salary Icon"
+                            />
+                            <p id='salary' className="ml-2 font-thin text-xsmall pl-px text-fontcolor">Php 17,000 - 21,000 Monthly</p>
+                        </div>
+                    </div>
+                </div>
+                {/* Saved Job #6 */}
+                <div className="flex flex-col items-center justify-center w-screen pt-4">
+                    <div className="box-container px-8 py-5 ">
+                        <div className="flex flex-row items-center -mt-8">
+                            <Image src="/Logo.png" width={30} height={30} alt="Company Logo" />
+                            <div className="ml-4 mt-8">
+                                <b className="font-bold text-large text-black">Job Title</b>
+                                    <div className="text-xsmall font-thin text-fontcolor mt-1">
+                                        <p>Company</p>
+                                        <p>Job Industry</p>
+                                    </div>
+                            </div>
+                            <div className="text-center ml-40">
+                                <a href="/APPLICANT/JobApplication" className="apply-now-container">Apply Now</a>
+                            </div>
+                            <button onClick={toggleSave} className="ml-auto">
+                                <Image src={isSaved ? "/Save Icon.svg" : "/Unsave Icon.svg"} width={13} height={11} alt={isSaved ? "Save Icon" : "Unsave Icon"} />
+                            </button>
+                        </div>  
+                        <div className="flex flex-row justify-between mt-2 px-11">
+                            <div className="flex flex-row">
+                                <Image src="/Location Icon.svg" width={23} height={20} alt="Location Icon" />
+                                <p id='work_location' className="ml-1.5 font-thin text-xsmall text-fontcolor">Valenzuela</p>
+                            </div>
+                            <div className="flex flex-row mx-4">
+                                <Image src="/Work Setup Icon.svg" width={23} height={20} alt="Work Setup Icon" />
+                                <p id='work_setup' className="ml-2 font-thin text-xsmall text-fontcolor">Remote</p>
+                            </div>
+                            <div className="flex flex-row">
+                                <Image src="/Schedule Icon.svg" width={18} height={20} alt="Schedule Icon" />
+                                <p id='schedule' className="ml-2 font-thin text-xsmall text-fontcolor">8 hrs shift</p>
+                            </div>
+                        </div>
+
+                        <div className="flex flex-row mt-2 px-11">
+                            <Image 
+                                src="/Salary Icon.svg" 
+                                width={18} 
+                                height={20}
+                                alt="Salary Icon"
+                            />
+                            <p id='salary' className="ml-2 font-thin text-xsmall pl-px text-fontcolor">Php 17,000 - 21,000 Monthly</p>
+                        </div>
+                    </div>
+                </div>
+            <GeneralFooter /></>
+        </div>
+    );
+}
+
+const SavedJobsWrapper = () => {
+    return (
+        <div className="flex overflow-y-auto border border-none hide-scrollbar p-1 h-[calc(100vh-150px)]">
+            <SavedJobs/>
+        </div>
+    );
+};
+
+export default SavedJobsWrapper;
--- frontend/pages/APPLICANT/JobApplication.js
+++ frontend/pages/APPLICANT/JobApplication.js
@@ -4,8 +4,8 @@ export default function  JobApplication () {
     return ( 
         <div>
             <ApplicantHeader/>
-                <div className=" lg:pt-28 mb:pt-24 xsm:pt-24 sm:pt-24 mb:px-20 sm:px-20 xsm:px-20 lg:px-20 mx-auto">
-                    <p>Job Application</p>
+                <div className=" lg:pt-28 mb:pt-24 xsm:pt-24 sm:pt-24 mb:px-20 sm:px-20 xsm:px-20 lg:px-20 mx-auto text-primary">
+                    <b>Job Application</b>
                 </div>
 
 
