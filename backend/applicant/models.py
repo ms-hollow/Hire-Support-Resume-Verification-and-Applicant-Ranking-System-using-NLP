a388db5dbc6c46ab4fc626fdfc8c236ae480c169
ms-hollow <leonen.abbegail@gmail.com>
2024-12-13 00:58:41 +0800

    wip: Setting up applicant profile form and data handling

--- backend/applicant/models.py
+++ backend/applicant/models.py
@@ -5,10 +5,15 @@ from users.models import User
 class Applicant(models.Model):
 
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    applicant_name = models.CharField(max_length=50, null=True, blank=False)
+    first_name = models.CharField(max_length=50, null=True, blank=False)
+    last_name = models.CharField(max_length=50, null=True, blank=False)
+    middle_name = models.CharField(max_length=50, null=True, blank=False)
     sex = models.CharField(max_length=10, choices=[('Male', 'Male'), ('Female', 'Female'), ('Others', 'Others')], null=True, blank=False)
     date_of_birth = models.DateField(null=True, blank=False)
     age = models.PositiveIntegerField(null=True, blank=False)
+    region = models.CharField(max_length=50, null=True, blank=False)
+    province = models.CharField(max_length=50, null=True, blank=False)
+    postal_code = models.CharField(max_length=50, null=True, blank=False)
     contact_number = models.CharField(null=True, blank=False)
     present_address = models.CharField(null=True, blank=False)
     linkedin_profile = models.URLField(null=True, blank=False)
