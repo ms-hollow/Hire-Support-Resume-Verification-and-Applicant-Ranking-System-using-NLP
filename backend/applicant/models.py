bfe3c7379697f22b00febbcf3e0873f989ba0d21
ms-hollow <leonen.abbegail@gmail.com>
2024-12-26 23:23:59 +0800

    feat: done with applicant profile

--- backend/applicant/models.py
+++ backend/applicant/models.py
@@ -8,16 +8,18 @@ class Applicant(models.Model):
     first_name = models.CharField(max_length=50, null=True, blank=False)
     last_name = models.CharField(max_length=50, null=True, blank=False)
     middle_name = models.CharField(max_length=50, null=True, blank=False)
-    sex = models.CharField(max_length=10, choices=[('Male', 'Male'), ('Female', 'Female'), ('Others', 'Others')], null=True, blank=False)
+    sex = models.CharField(max_length=10, null=True, blank=False)
     date_of_birth = models.DateField(null=True, blank=False)
     age = models.PositiveIntegerField(null=True, blank=False)
     region = models.CharField(max_length=50, null=True, blank=False)
     province = models.CharField(max_length=50, null=True, blank=False)
     postal_code = models.CharField(max_length=50, null=True, blank=False)
+    city = models.CharField(max_length=50, null=True, blank=False)
     contact_number = models.CharField(null=True, blank=False)
+    barangay = models.CharField(null=True, blank=False)
     present_address = models.CharField(null=True, blank=False)
     linkedin_profile = models.URLField(null=True, blank=False)
     saved_jobs = models.ManyToManyField('jobs.JobHiring', related_name='saved_by', blank=True)
 
     def __str__(self):
-        return f"{self.applicant_name}"
+        return f"{self.first_name} {self.middle_name} {self.last_name}"
